63334e260a2fc57b60a1d850649e02b879f72e32
veasna72 <82855589+veasna72@users.noreply.github.com>
2021-04-30 13:43:35 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+file_path = "r6.jpg"
+
+img = PIL.Image.open(file_path)
+
+myHeight, myWidth = img.size
+
+img = img.resize((myHeight, myWidth), PIL.Image.ANTIALIAS)
+
+save_path = "r6_1.jpg"
+if((os.path.getsize(file_path))>1000000):
+    print(1)
+    img.save(save_path, optimize=True,quality=25)
+else:
+    print(2)
+    img.save(save_path, optimize=True,quality=50)
