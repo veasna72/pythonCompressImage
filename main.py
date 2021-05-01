6050fc5a841eef206eca2028179b784c0d716380
veasna72 <82855589+veasna72@users.noreply.github.com>
2021-05-01 12:50:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import os
+import time
+from flask import Flask
+app = Flask(__name__)
+
+START = time.time()
+
+def elapsed():
+    running = time.time() - START
+    minutes, seconds = divmod(running, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+@app.route('/')
+def root():
+    return "Hello World (Python)! (up %s)\n" % elapsed()
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
 # import PIL
 # from PIL import Image
 # from tkinter.filedialog import *
@@ -19,4 +37,4 @@ import os
 #     print(2)
 #     img.save(save_path, optimize=True,quality=50)
 
-print("hello world")
+
